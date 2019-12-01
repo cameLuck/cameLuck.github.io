c25add0bfcf0006d92728aa44e5486e605c42ee6
cameLuck <cameluck@emailn.de>
2019-12-01 20:12:49 +0100

    add image

--- 1.1/classes/Pipe/index.php
+++ 1.1/classes/Pipe/index.php
@@ -2,4 +2,10 @@
 $data = file_get_contents('./pipes.json', true);
 $data = json_decode($data, true);
 header('Content-Type: application/json');
-echo json_encode($data);
+header('server: nginx');                                                                   
+header('content-type: application/json;charset=utf-8');
+header('vary: Accept-Encoding');
+header('cache-control: no-cache,no-store');
+header('pragma: no-cache');
+header('content-encoding: gzip'); 
+echo gzencode(json_encode($data));
